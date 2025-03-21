ff360252a548f4c365154d47e407533216ce052a
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-03-21 12:22:01 -0300

    feat: login screen has been added

---
+++ app/src/main/java/com/example/appparametros/ui/screen/LoginScreen.kt
@@ -0,0 +1,44 @@
+package com.example.appparametros.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(navController: NavController) {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Você está na tela de login", modifier = Modifier.padding(top = 16.dp))
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.padding(top = 16.dp),
+            label = { Text(text = "Digite seu nome de usuário...") }
+        )
+
+        Button(onClick = {
+            val name = text.ifEmpty { "Cara" }
+            navController.navigate("home/$name")
+        }, modifier = Modifier.padding(16.dp)) {
+            Text(text = "Navegar para Home")
+        }
+    }
+}
