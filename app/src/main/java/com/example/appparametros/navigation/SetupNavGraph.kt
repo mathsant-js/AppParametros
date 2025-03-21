d975770bcbb230b9fd1ce7b30bca48c5ece601d9
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-03-21 12:12:43 -0300

    feat: setup for navigation has been added

---
+++ app/src/main/java/com/example/appparametros/navigation/SetupNavGraph.kt
@@ -0,0 +1,28 @@
+package com.example.appparametros.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+
+@Composable
+fun SetupNavGraph(navController: NavController) {
+    NavHost(navController = navController, startDestination = Screens.LoginScreen.route) {
+        composable(route = Screens.LoginScreen.route) {
+            LoginScreen(navController = navController)
+        }
+
+        composable(route = Screens.HomeScreen.route, arguments = listOf(
+            navArgument(USER) {
+                type = NavType.StringType
+                defaultValue = ""
+                nullable = true
+            }
+        )) {
+            HomeRoute(userName = it.arguments?.getString(USER) ?: "")
+        }
+    }
+}
