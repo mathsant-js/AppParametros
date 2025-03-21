9805a6af1ece244c65a39bf94ded47c47c5ac9c3
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-03-21 12:24:40 -0300

    feat: login screen preview has been added

--- app/src/main/java/com/example/appparametros/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/appparametros/ui/screen/LoginScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,5 +42,13 @@ fun LoginScreen(navController: NavController) {
         }, modifier = Modifier.padding(16.dp)) {
             Text(text = "Navegar para Home")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    NavigationTheme {
+        LoginScreen(navController = rememberNavController())
     }
 }
