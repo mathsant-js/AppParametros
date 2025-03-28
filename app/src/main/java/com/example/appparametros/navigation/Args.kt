eb7cecf6929455dd1b08d4a52dc8c831d017d166
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-03-28 11:48:45 -0300

    feat: args and routes has been added

---
+++ app/src/main/java/com/example/appparametros/navigation/Args.kt
@@ -0,0 +1,3 @@
+package com.example.appparametros.navigation
+
+const val USER = "user"
---
+++ app/src/main/java/com/example/appparametros/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.appparametros.navigation
+
+object Routes {
+    const val LOGIN_ROUTE = "login"
+    const val HOME_ROUTE = "home/{user}"
+}
