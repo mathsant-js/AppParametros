52494ba32ccd2fee81807abb254e148e1a965e22
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-04-04 11:42:39 -0300

    feat & fix: user images has been added in the system and name images has been fixed

--- app/src/main/java/com/example/appparametros/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/appparametros/ui/screen/HomeScreen.kt
@@ -1,34 +1,74 @@
 package com.example.appparametros.ui.screen
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.appparametros.R
 import com.example.appparametros.data.User
 import com.example.appparametros.data.lucasTino
 import com.example.appparametros.ui.theme.AppParametrosTheme
 import com.example.appparametros.ui.theme.Orange
+import com.example.appparametros.ui.theme.OrangeIntermediary
 
 
+@SuppressLint("DiscouragedApi")
 @Composable
 fun HomeScreen(user: User, navController: NavController) {
+    val context = LocalContext.current
+    val img = "@drawable/" + "${user.avatarUrl}"
+    val imageResource: Int = context.resources.getIdentifier(img, null, context.packageName)
+
     Column(
         Modifier
             .fillMaxWidth()
             .background(Color(23,23,23)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Orange, OrangeIntermediary, Color.White
+                        )
+                    )
+                )
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = "Descrição da Imagem",
+                modifier = Modifier
+                    .offset(y = (10).dp)
+                    .clip(shape = CircleShape)
+                    .align(Alignment.Center)
+                    .size(150.dp)
+            )
+        }
         Text(
             text = "Você está na tela Home",
             color = Orange,
@@ -74,13 +114,5 @@ fun HomeScreen(user: User, navController: NavController) {
         )
 
         Text(text = "Idade: ${user.age}", modifier = Modifier.padding(top = 4.dp), color = Orange, fontSize = 16.sp)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomePreview() {
-    AppParametrosTheme {
-        HomeScreen(user = lucasTino, navController = rememberNavController())
     }
 }
